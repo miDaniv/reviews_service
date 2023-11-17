ab6eed4ede10f1859b4b8e94579db42bb5efcd07
miDaniv <mishami242@gmail.com>
2023-11-17 21:53:48 +0200

    test sql

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 var http = require('http');
+const mysql = require('mysql');
+
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -7,12 +9,24 @@ var con = mysql.createConnection({
   password: "password"
 });
 
+
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }).listen(8080);
+
+
+con.connect((err) => {
+  if (err) {
+    console.error('Помилка підключення до бази даних:', err);
+  } else {
+    console.log('Підключено до бази даних');
+  }
+});
+
