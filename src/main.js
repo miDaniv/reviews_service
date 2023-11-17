652c2c503678ea1e00c99a3e0495e211d3aaa9c5
miDaniv <mishami242@gmail.com>
2023-11-17 21:35:59 +0200

    test

--- src/main.js
+++ src/main.js
@@ -10,3 +10,14 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Привіт, світ!');
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущено на порту ${port}`);
+});
+
